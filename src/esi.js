e67be91cd2c5682c694a7010d339685983f54ed4
Andrew Dodson <andrew.j.dodson@gmail.com>
2014-07-09 09:03:12 +1000

    Optimize ESI fragments, using Array.map and getting rid of the cruft

--- src/esi.js
+++ src/esi.js
@@ -8,12 +8,18 @@ var http = require('http');
 
 
 
-var reg_esi_tags = /<(esi\:include)\s([^>]+)>([\s\S]*?)<\/\1>/ig,
-	reg_esi_tag_split = /<esi\:include\s[^>]+>[\s\S]*?<\/esi\:include>/ig,
-	reg_esi_tag = /<(esi\:include)\s([^>]+)>([\s\S]*?)<\/\1>/i;
+var reg_esi_tag_split = /(<esi\:include\s[^>]+>[\s\S]*?<\/esi\:include>)/ig;
 
 
-module.exports = function ESI( body, encoding ){
+module.exports = ESI;
+
+
+
+// The main point of entry
+// Given an body of text, break it down into strings and Promise objects
+// @return Promise
+
+function ESI( body, encoding ){
 
 	// Format incoming
 
@@ -21,93 +27,87 @@ module.exports = function ESI( body, encoding ){
 		body = (body || '').toString();
 	}
 
+	// Split the current string into parts, some including the ESI fragment and then the bits in between
+	// Loop through and process each of the ESI fragments, mapping them back to a parts array containing strings and the Promise objects
 
-	// Create a series of Promise object
-
-	var tag_promises = [];
-
-
-	// Find matches in the body which contain the ESI fragment
-
-	var tags = body.match(reg_esi_tags) || [];
-
-
-	// Split the current string into parts
-
-	var parts = body.split(reg_esi_tag_split);
-
-
-	// Loop through and process each of the ESI fragments
-
-	tags.forEach(function(str){
-
-
-		// Get Tag Attributes in an object
-
-		var m = str.match(reg_esi_tag);
-
-		var attrs = getAttributes(m[2]);
-
-
-		// Create a new Promise object for this tag and add it to the Promise Array
-		tag_promises.push( new Promise(function(resolve, reject){
-
-			if( !attrs.src ){
-
-				// Urgh this should have contained a src attibute
-				// Just spit it back
-				resolve(str);
-				return;
-
-			}
-
-			http.get( attrs.src, function(res){
-
-				var body='';
-
-				res.on('data', function(buffer){
-					body += buffer;
-				});
-
-				res.on('end', function(){
-
-					// Check whether the response also contains ESI fragments
-
-					var esi = ESI( body, encoding );
-
-					// resolve this Promise with the response from the server
-					esi.then(resolve);
-
-				});
-
-
-			});
-
-		}));
-
-	});
+	var parts = body.split( reg_esi_tag_split ).map( processESITags );
 
 
 
 	// Create the mother of all promises, to process all the child operations into a single resolve.
 	
-	return Promise.all(tag_promises).then(function(response){
+	return Promise.all(parts).then(function(response){
 
-		// Once all these have returned we can replace the parts of the body with the ESI fragments
-		var r = [];
-
-		for(var i=0; i < parts.length; i++){
-			// One from parts
-			r.push(parts[i]);
-			// The other from
-			if(i in response){
-				r.push(response[i]||'');
-			}
-		}
-
-		return r.join('');
+		// Once all these have returned we can merge the parts together
+		return response.join('');
 	});
-};
+}
+
+
+
+
+
+var reg_esi_tag = /<(esi\:include)\s([^>]+)>([\s\S]*?)<\/\1>/i;
+
+
+// Process ESI tags
+// Given a section of the body string, if it is a esi tag process it and return a Promise object, otherwise return a string.
+
+function processESITags(str){
+
+
+	// Get Tag Attributes in an object
+
+	var m = str.match(reg_esi_tag);
+
+
+	// Is this a tag?
+	if( !m ){
+		// nope, spit it back...
+		return str;
+	}
+
+
+	var attrs = getAttributes(m[2]);
+
+
+	if( !attrs.src ){
+
+		// Urgh this should have contained a src attibute
+		// Just spit it back, its not in a correct format
+		return str;
+	}
+
+
+	// Replace the section with a new Promise object for this tag and add it to the Promise Array
+	return new Promise(function(resolve, reject){
+
+		http.get( attrs.src, function(res){
+
+			var body='';
+
+			res.on('data', function(buffer){
+				body += buffer;
+			});
+
+			res.on('end', function(){
+
+				// Check whether the response also contains ESI fragments
+
+				var esi = ESI( body );
+
+				// resolve this Promise with the response from the server
+				esi.then(resolve);
+
+			});
+
+
+		});
+
+	});
+
+}
+
 
 
 // getAttributes
